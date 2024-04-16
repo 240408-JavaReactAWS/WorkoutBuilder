948651c24f509943b3086904b2d311e9acccedcc
bryanserfozo <bryan.serfozo@revature.com>
2024-04-16 13:30:23 -0400

    Pushing plan user stories

--- src/main/java/com/revature/controllers/PlanController.java
+++ src/main/java/com/revature/controllers/PlanController.java
@@ -1,15 +1,14 @@
 package com.revature.controllers;
 
+import com.revature.exceptions.InvalidCredentialsException;
+import com.revature.exceptions.NoSuchPlanException;
 import com.revature.exceptions.NoSuchUserException;
 import com.revature.models.Plan;
 import com.revature.services.PlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -42,5 +41,71 @@ public class PlanController {
         }
 
         return new ResponseEntity<>(plans, HttpStatus.OK);
+    }
+
+    @GetMapping("id")
+    public ResponseEntity<Plan> getPlanByIdAndUserHandler(
+            @RequestHeader(name = "user", required = false) String username,
+            @PathVariable int id
+    ){
+        if (username.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Plan returnedPlan;
+
+        try{
+            returnedPlan = ps.getPlanByUserAndId(username, id);
+        } catch( InvalidCredentialsException e){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        } catch (NoSuchUserException | NoSuchPlanException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(returnedPlan, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Plan> updatePlanByUserHandler(
+            @RequestHeader(name = "user", required = false) String username,
+            @RequestBody Plan plan
+    ){
+        if (username.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Plan updatedPlan;
+
+        try{
+            updatedPlan = ps.updatePlanByUser(plan, username);
+        } catch( InvalidCredentialsException e){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        } catch (NoSuchUserException | NoSuchPlanException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(updatedPlan, HttpStatus.OK);
+    }
+
+    @DeleteMapping("id")
+    public ResponseEntity<Object> deletePlansByUserHandler(
+            @RequestHeader(name = "user", required = false) String username,
+            @PathVariable int id
+    ){
+        if (username.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        boolean successfullyDeleted = false;
+
+        try{
+            successfullyDeleted = ps.deleteByUserAndId(username, id);
+        } catch( InvalidCredentialsException e){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        } catch (NoSuchUserException | NoSuchPlanException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(successfullyDeleted, HttpStatus.OK);
     }
 }
--- src/main/java/com/revature/services/PlanService.java
+++ src/main/java/com/revature/services/PlanService.java
@@ -1,5 +1,7 @@
 package com.revature.services;
 
+import com.revature.exceptions.InvalidCredentialsException;
+import com.revature.exceptions.NoSuchPlanException;
 import com.revature.exceptions.NoSuchUserException;
 import com.revature.models.Plan;
 import com.revature.models.User;
@@ -35,5 +37,82 @@ public class PlanService {
 
         return pd.getPlansByOwner(returnedUser);
 
+    }
+
+    public Plan updatePlanByUser(Plan plan, String username) throws
+            NoSuchUserException, InvalidCredentialsException, NoSuchPlanException {
+        Optional<User> possibleUser = ud.findUserByUsername(username);
+
+        if (possibleUser.isEmpty()){
+            throw new NoSuchUserException("No user found with username: " + username);
+        }
+        User returnedUser = possibleUser.get();
+
+        Optional<Plan> possiblePlan = pd.findById(plan.getPlanId());
+
+        if (possiblePlan.isEmpty()){
+            throw new NoSuchPlanException("No plan found with id: " + plan.getPlanId());
+        }
+
+        Plan returnedPlan = possiblePlan.get();
+
+        if (returnedPlan.getOwner().equals(returnedUser)){
+            // This means the proper user is accessing this piece
+            return pd.save(plan);
+        }
+        throw new InvalidCredentialsException("Incorrect user attempting to modify workout plan");
+
+
+    }
+
+    public boolean deleteByUserAndId(String username, int id) throws
+            NoSuchUserException, InvalidCredentialsException, NoSuchPlanException{
+
+        Optional<User> possibleUser = ud.findUserByUsername(username);
+
+        if (possibleUser.isEmpty()){
+            throw new NoSuchUserException("No user found with username: " + username);
+        }
+        User returnedUser = possibleUser.get();
+
+        Optional<Plan> possiblePlan = pd.findById(id);
+
+        if (possiblePlan.isEmpty()){
+            throw new NoSuchPlanException("No plan found with id: " + id);
+        }
+
+        Plan returnedPlan = possiblePlan.get();
+
+        if (returnedPlan.getOwner().equals(returnedUser)){
+            // This means the proper user is accessing this piece
+            pd.delete(returnedPlan);
+            return !pd.existsById(id);
+        }
+        throw new InvalidCredentialsException("Incorrect user attempting to delete workout plan");
+    }
+
+    public Plan getPlanByUserAndId(String username, int id) throws
+            NoSuchUserException, InvalidCredentialsException, NoSuchPlanException{
+
+        Optional<User> possibleUser = ud.findUserByUsername(username);
+
+        if (possibleUser.isEmpty()){
+            throw new NoSuchUserException("No user found with username: " + username);
+        }
+        User returnedUser = possibleUser.get();
+
+        Optional<Plan> possiblePlan = pd.findById(id);
+
+        if (possiblePlan.isEmpty()){
+            throw new NoSuchPlanException("No plan found with id: " + id);
+        }
+
+        Plan returnedPlan = possiblePlan.get();
+
+        if (returnedPlan.getOwner().equals(returnedUser)){
+            // This means the proper user is accessing this piece
+            return returnedPlan;
+        }
+        throw new InvalidCredentialsException("Incorrect user attempting to access workout plan");
     }
 }
